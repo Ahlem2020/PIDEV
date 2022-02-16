8d0fea9fced1eae9e69bdfdc9a94e2425b6181d7
Hadj-Said-Ahmed <ahmed.hadjsaid@esprit.tn>
2022-02-16 19:01:07 +0100

    Initiation

---
+++ src/main/java/tn/esprit/spring/entities/Training.java
@@ -0,0 +1,35 @@
+package tn.esprit.spring.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Training implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String subject;
+	private Date startDate;
+	private Date endDate;
+	private int nbreMax;
+	private int nbreParticipation;
+	
+	
+
+}
